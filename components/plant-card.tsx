a657e525b259798aab59caa75e43c39ce0478a1d
kenVis1529 <tathuctrungkien2003@gmail.com>
2024-10-16 19:51:29 +0700

    Redesign

--- components/plant-card.tsx
+++ components/plant-card.tsx
@@ -9,21 +9,23 @@ export const PlantCard = ({ plant }: { plant: PlantSchema }) => {
   return (
     <div className="w-full flex gap-x-2 px-6">
       <CardWrapper plant={plant} className="flex-1">
-        <div>
-          <div className="text-xl font-semibold text-center py-6">
+        <div className="pb-6">
+          <div className="text-xl font-semibold text-center py-2">
             Trạng thái hiện tại
           </div>
-          <div className="w-full flex items-center justify-around object-contain overflow-x-auto pb-4">
-            <PercentageCol type="humidity" value={plant.humidity} />
-            <PercentageCol type="moisture" value={plant.moisture} />
-            <PercentageCol type="temperature" value={plant.temperature} />
-            <PercentageCol type="light" value={plant.light} />
+          <div className="border p-4 rounded-lg">
+            <div className="w-full flex items-center justify-around object-contain overflow-x-auto">
+              <PercentageCol type="humidity" value={plant.humidity} />
+              <PercentageCol type="moisture" value={plant.moisture} />
+              <PercentageCol type="temperature" value={plant.temperature} />
+              <PercentageCol type="light" value={plant.light} />
+            </div>
           </div>
         </div>
         <div>
-          <div className="flex flex-col items-center justify-center mx-auto 2xl:w-[800px]">
-            <div className="text-lg font-semibold py-6">Biểu đồ theo giờ</div>
-            <div className="w-full ">
+          <div className="flex flex-col gap-2 items-center justify-center mx-auto 2xl:w-[800px]">
+            <div className="text-xl font-semibold py-2">Biểu đồ theo giờ</div>
+            <div className="w-full border rounded-lg p-4">
               <div>Nhiệt độ không khí</div>
               <PlantLineChart
                 plantId={plant.id}
@@ -31,7 +33,7 @@ export const PlantCard = ({ plant }: { plant: PlantSchema }) => {
                 type="temperature"
               />
             </div>
-            <div className="w-full">
+            <div className="w-full border rounded-lg p-4">
               <div>Độ ẩm không khí</div>
               <PlantLineChart
                 plantId={plant.id}
@@ -39,7 +41,7 @@ export const PlantCard = ({ plant }: { plant: PlantSchema }) => {
                 type="humidity"
               />
             </div>
-            <div className="w-full">
+            <div className="w-full border rounded-lg p-4">
               <div>Độ ẩm đất</div>
               <PlantLineChart
                 plantId={plant.id}
@@ -47,7 +49,7 @@ export const PlantCard = ({ plant }: { plant: PlantSchema }) => {
                 type="moisture"
               />
             </div>
-            <div className="w-full">
+            <div className="w-full border rounded-lg p-4">
               <div>Cường độ ánh sáng</div>
               <PlantLineChart plantId={plant.id} hours={hours} type="light" />
             </div>
